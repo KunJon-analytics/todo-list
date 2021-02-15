ee58548c3091718b9264f7b1b9dade8814953bc6
KunJon Analytics <johnadelakun1@gmail.com>
2021-02-15 18:12:04 +0100

    2. added the frontend

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Have you coded for Pilife or Realestate?");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
